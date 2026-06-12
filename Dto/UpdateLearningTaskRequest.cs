d42f0e0c70efe5fa197d336011707cbf433767d7
michael <michael.hosamani@zeuslearning.com>
2026-06-12 13:41:52 +0530

    fix: remove required attribute in updateLearningTaskRequest Dto

--- Dto/UpdateLearningTaskRequest.cs
+++ Dto/UpdateLearningTaskRequest.cs
@@ -5,19 +5,14 @@ namespace TraineeManagementApi.Dto;
  
 public class UpdateLearningTaskRequest
 {   
-    [Required(ErrorMessage = "Learning task title is required")] 
-    public required string? Title { get; set; }
+    public string? Title { get; set; }
 
-    [Required(ErrorMessage = "Learning task description is required")] 
-    public required string? Description { get; set; }
+    public string? Description { get; set; }
 
-    [Required(ErrorMessage = "Expected tech stack is required")] 
-    public required string?  ExpectedTechStack { get; set; }
+    public string?  ExpectedTechStack { get; set; }
 
-    [Required(ErrorMessage = "Due date is required")] 
-    public required DateTime? DueDate { get; set; }
+    public DateTime? DueDate { get; set; }
 
-    [Required(ErrorMessage = "Status is required")]
     [EnumDataType(typeof(LearningTaskStatus), ErrorMessage = "Status must be valid")]
-    public required LearningTaskStatus? Status { get; set; }
+    public LearningTaskStatus? Status { get; set; }
 }
